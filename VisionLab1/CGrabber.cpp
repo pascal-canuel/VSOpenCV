2d12afa312884d746e1ecd810c7a9690fdebf543
Pascal <pascalcanuel@hotmail.com>
2018-08-24 16:44:59 -0400

    set black pixel if its not red

--- VisionLab1/CGrabber.cpp
+++ VisionLab1/CGrabber.cpp
@@ -1,2 +1,37 @@
 #include "pch.h"
 #include "CGrabber.h"
+
+// Member functions definitions including constructor
+CGrabber::CGrabber(void) {
+	cout << "Loaded" << endl;
+}
+void CGrabber::setColor(Mat pColor) {
+	_color = pColor;
+}
+void CGrabber::getColor(Mat pColor) {
+	// Check if red is present in the picture
+	bool isRed = false; 
+	_nbImage = 0;
+
+	for (int i = 0; i < pColor.rows; i++) {
+		for (int j = 0; j < pColor.cols; j++) {
+			if ((int)pColor.at<Vec3b>(i, j)[2] > 100 && (int)pColor.at<Vec3b>(i, j)[1] < 40 && (int)pColor.at<Vec3b>(i, j)[0] < 40) {
+				isRed = true;
+			}
+			else {
+				pColor.at<Vec3b>(i, j)[2] = 0;
+				pColor.at<Vec3b>(i, j)[1] = 0;
+				pColor.at<Vec3b>(i, j)[0] = 0;
+			}
+		}	
+	}
+	
+	//std::cout << (int)pColor.at<Vec3b>(i, j)[0] << " " << (int)pColor.at<Vec3b>(i, j)[1] << " " << (int)pColor.at<Vec3b>(i, j)[2] << std::endl;
+	if (isRed) {
+		cout << "Red!";
+
+		imshow("its red looolll", pColor);
+		_nbImage++;
+	}
+}
+
--- VisionLab1/CGrabber.h
+++ VisionLab1/CGrabber.h
@@ -1,1 +1,17 @@
 #pragma once
+#include "pch.h"
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+
+class CGrabber {
+public:
+	void setColor(Mat pColor);
+	void getColor(Mat pColor);
+	CGrabber();  // This is the constructor
+private:
+	Mat _color;
+	int _nbImage;
+};
